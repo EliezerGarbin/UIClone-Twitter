65a662fe15835c77cae559757ec00e43749f2e02
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-07-28 18:28:23 -0300

    Criando components feed

--- twitter-clone/src/components/ProfilePage/index.tsx
+++ twitter-clone/src/components/ProfilePage/index.tsx
@@ -7,7 +7,8 @@ import {
   ProfileData, 
   LocationIcon, 
   CakeIcon, 
-  Followage 
+  Followage,
+  EditButton
 } from './styles';
 
 const ProfilePage: React.FC = () => {
@@ -17,7 +18,7 @@ const ProfilePage: React.FC = () => {
         <Avatar />
       </Banner>
       <ProfileData>
-        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        <EditButton outlined>Editar perfil</EditButton>
         
         <h1>ELiezer G.</h1>
         <h2>@EliezerGarbin</h2>
@@ -46,6 +47,8 @@ const ProfilePage: React.FC = () => {
           </span>
         </Followage>
       </ProfileData>
+
+      <Feed />
     </Container>
   );
 };
